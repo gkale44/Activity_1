19dc0551feaad4d2037fa6c62a2eec162f24391c
Ganesh Kale <42440804+gkale44@users.noreply.github.com>
2020-01-10 12:29:09 +0530

    Update Sum_of_array_and_swap.c

--- Sum_of_array_and_swap.c
+++ Sum_of_array_and_swap.c
@@ -1,5 +1,6 @@
 // SUM OF ELEMENTS OF AN ARRAY
 #include<stdio.h>
+void swap (int,int)
 int main()
 {
     int a[5],i,sum=0,x;
@@ -12,3 +13,16 @@ int main()
     printf("Sum of element of given array=%d\n",sum);
 
 }
+	void swap(int,int);
+{
+	  int a,b;
+	  a=10;
+	  b=20;
+	  swap(a,b);
+	  printf("a=%d,b=%d\n",a,b);	  return 0;
+	}
+	void swap(int x,int y) {
+  int t=x;
+	  x=y;
+	  y=t;
+	}
